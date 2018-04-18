be3750c72ec5e574cfec34ac0eef134cf23cd230
Gulmira Berdalieva <gulmira2410@gmail.com>
2018-04-18 10:48:49 -0700

    db schema change

--- src/models/db/schema.sql
+++ src/models/db/schema.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS books;
 CREATE TABLE books (
   id SERIAL PRIMARY KEY,
   title VARCHAR(255) NOT NULL,
-  author VARCHAR(255) NOT NULL,
+  author VARCHAR(255),
   genre VARCHAR(255),
   height INT,
   publisher VARCHAR(255)
